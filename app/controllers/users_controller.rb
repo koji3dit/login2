fbdd4021aa5020f636064e105272fd5fc76961fc
koujikawahara <info_3d@3dit.jp>
2016-10-17 13:14:44 +0000

    #6 user_controller changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+    before_action :reject_without_admin
+
   def index
      @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def reject_without_admin
+    redirect_to root_path unless (current_user && current_user.admin == true)
   end
 end
