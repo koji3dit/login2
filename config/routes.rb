b3616bc0c77691c31ff09e161bdfadcb52bee428
tomoaki-kimura <tkim19750404@gmail.com>
2016-10-28 11:32:12 +0900

    #12 add edit update

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'users/:id/admin_new', to: 'profiles#admin_new', as: 'new_admin_profile'
   post 'users/:id/admin_create', to: 'profiles#admin_create', as: 'admin_profile'
 
-  resources :addresses, only: [:new, :create]
+  resources :addresses
   post 'addresses/serch_address', to: 'addresses#serch_address', as: 'serch_address'
 
   # resources :simei
